b9d0141e9e123175f73f0a92c733dfeb7c0785dc
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-04-03 18:03:59 -0700

    configured AWS tokens

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
+var AWS = require("aws-sdk");
 
 app.set("port", process.env.PORT || 3001);
+
+const AWS_ACESSS_KEY = process.env.aws_access_key;
+const AWS_SECRET_KEY = process.env.aws_secret_key;
+
+const AWSConfig = {
+    accessKeyId: AWS_ACESSS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+}
+
+AWS.config.update(AWSConfig);
+
 
 app.listen(app.get("port"), () => {
     console.log(`Server at: http://localhost:${app.get("port")}/`);
