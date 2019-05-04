0ac19bef86bf4a2ca1b6486d9b4c7c2abec5292b
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-05-04 13:08:25 -0700

    added extra questions and modal with redirects to dashboard

--- client/src/containers/firstTime.js
+++ client/src/containers/firstTime.js
@@ -1,27 +1,56 @@
 import React, { Component } from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   Redirect,
-//   Switch
-// } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import { Button, Header, Image, Modal } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./firstTime.css";
+import userPhoto from "../assets/icon.png";
 
 let data = require("../questions.json");
 console.log(data.Questions[0].answers[0]);
+
+let level = 1;
+const ConfirmationModal = props => (
+  <Modal trigger={props.trigger}>
+    <Modal.Header>Confirmation</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size="medium" src={userPhoto} />
+      <Modal.Description>
+        <Header>Survey Completed!</Header>
+        <div>
+          <p>
+            We got all the information needed to match you to others! Your
+            current level is displayed below. Do not get discouraged if it is
+            lower than expected but this will help you learn with other people
+            at a similar experience level. Best of luck!
+          </p>
+          <p>Current level {level} </p>
+          <button onClick={props.clickMethod} className={"submitBtn"}>
+            Continue
+          </button>
+        </div>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
+);
 
 class FirstTimeUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
-      quiz: []
+      quiz: [],
+      showModal: false
     };
 
     this.handleNextQuestion = this.handleNextQuestion.bind(this);
+    this.handleDashBoardRedirect = this.handleDashBoardRedirect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleNextQuestion = () => {
@@ -46,6 +75,14 @@ class FirstTimeUser extends Component {
     // and remove firstTime user
   };
 
+  handleSubmit = () => {
+    this.setState({ showModal: true });
+  };
+  handleDashBoardRedirect = () => {
+    console.log("clicked redirect");
+    window.location = "/dashboard";
+  };
+
   render() {
     let i = this.state.count;
     let question;
@@ -58,29 +95,27 @@ class FirstTimeUser extends Component {
       );
       nextQuestionBtn = (
         <button onClick={this.handleNextQuestion} className="nextQuestionBtn">
-          {" "}
           Next Question
         </button>
       );
       form = (
         <div>
-          {" "}
           <span className="option">
             <button onClick={this.handleButtonPressed} className="answer">
               A
-            </button>{" "}
+            </button>
             {data.Questions[i].answers[0].a}
           </span>
           <span className="option">
             <button onClick={this.handleButtonPressed} className="answer">
               B
-            </button>{" "}
+            </button>
             {data.Questions[i].answers[0].b}
           </span>
           <span className="option">
             <button onClick={this.handleButtonPressed} className="answer">
               C
-            </button>{" "}
+            </button>
             {data.Questions[i].answers[0].c}
           </span>
           <span onClick={this.handleButtonPressed} className="option">
@@ -91,7 +126,11 @@ class FirstTimeUser extends Component {
       );
       if (i === data.Questions.length - 1) {
         nextQuestionBtn = null;
-        submitBtn = <button>Submit</button>;
+        submitBtn = (
+          <button onClick={this.handleSubmit} className={"submitBtn"}>
+            Submit
+          </button>
+        );
       }
     }
     return (
@@ -99,7 +138,11 @@ class FirstTimeUser extends Component {
         {question}
         <div className="questionNavigation">{nextQuestionBtn}</div>
         {form}
-        {submitBtn}
+        <ConfirmationModal
+          clickMethod={this.handleDashBoardRedirect}
+          trigger={submitBtn}
+          open={this.state.showModal}
+        />
       </div>
     );
   }
